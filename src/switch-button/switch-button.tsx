ba4c420059f3cc3e47403f170535235778e7234d
Donn Williams <donn.williams@feelbelt.de>
2023-10-18 15:01:33 +0200

    Update package

--- src/switch-button/switch-button.tsx
+++ src/switch-button/switch-button.tsx
@@ -4,9 +4,14 @@ import { Switch, type SxProps, type Theme } from '@mui/material'
 interface SwitchButtonProps {
   onToggle: (checked: boolean) => void
   checked: boolean
+  bgTrackChecked: string
+  bgTrackUnChecked: string
+  bgThumbCheckedBefore: string
+  bgThumbUnCheckedBefore: string
+  bgThumbCheckedAfter: string
 }
 
-export const SwitchButton: React.FC<SwitchButtonProps> = ({ onToggle, checked }) => {
+export const SwitchButton: React.FC<SwitchButtonProps> = ({ onToggle, checked, bgTrackChecked,bgTrackUnChecked, bgThumbCheckedBefore, bgThumbUnCheckedBefore, bgThumbCheckedAfter  }) => {
   const handleToggle = (event: React.ChangeEvent<HTMLInputElement>): void => {
     const newCheckedState = event.target.checked
     onToggle(newCheckedState)
@@ -14,7 +19,7 @@ export const SwitchButton: React.FC<SwitchButtonProps> = ({ onToggle, checked })
 
   return (
     <Switch
-      sx={switchStyle(checked)}
+      sx={switchStyle(checked, bgTrackChecked, bgTrackUnChecked, bgThumbCheckedBefore, bgThumbUnCheckedBefore, bgThumbCheckedAfter)}
       style={{ background: 'transparent' }}
       checked={checked}
       onChange={handleToggle}
@@ -22,16 +27,16 @@ export const SwitchButton: React.FC<SwitchButtonProps> = ({ onToggle, checked })
   )
 }
 
-function switchStyle (checked: boolean): SxProps<Theme> {
+function switchStyle (checked: boolean, bgTrackChecked: string, bgTrackUnChecked: string, bgThumbCheckedBefore: string, bgThumbUnCheckedBefore: string,bgThumbCheckedAfter: string): SxProps<Theme> {
   return {
     '& .MuiSwitch-track': {
-      background: checked ? '#4C30E8' : '#2D3339'
+      background: checked ? bgTrackChecked : bgTrackUnChecked
     },
     '& .MuiSwitch-thumb': {
-      backgroundColor: checked ? '#8275F0' : '#454D54',
+      backgroundColor: checked ? bgThumbCheckedBefore : bgThumbUnCheckedBefore,
       '&:after': {
         // eslint-disable-next-line max-len
-        backgroundImage: checked ? `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="14" width="14" viewBox="0 0 24 24"><path fill="${encodeURIComponent('#FFFFFF')}" d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z"/></svg>')` : ''
+        backgroundImage: checked ? `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="14" width="14" viewBox="0 0 24 24"><path fill="${encodeURIComponent(bgThumbCheckedAfter)}" d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z"/></svg>')` : ''
       }
     }
   }
